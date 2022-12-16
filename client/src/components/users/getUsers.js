016b7786cfaf94617e8e325eb6d0787a5082a837
Lauren Sousa <thelaurensousa@gmail.com>
2022-12-16 14:13:41 -0500

    Updated getUsers

--- client/src/components/users/getUsers.js
+++ client/src/components/users/getUsers.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 
 const getUsers = () => {
-    return axios.get('http://localhost:8000/users')
+    const listOfUsers = [];
+    axios.get('http://localhost:8000/users')
+        .then((res) => {
+            const userData = res.data
+            for (const key of Object.keys(userData)) {
+                listOfUsers.push({id: userData[key]._id, username: userData[key].username})
+            }
+        })
+        .catch((err) => console.log(err));
+    return listOfUsers;
 }
 
 export default getUsers;
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../App.css';
 import Nav from '../components/nav';
-import UserList from '../components/userList';
 
 export default () => {
     const username = localStorage.getItem('username');
@@ -10,7 +9,6 @@ export default () => {
         <div>
             <Nav/>
             <h1>Welcome {username}!</h1>
-            <UserList/>
         </div>
     );
 }

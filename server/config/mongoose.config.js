e5ef067cc2d84a6c51c08e503f28f8ab6546db0c
Lauren Sousa <thelaurensousa@gmail.com>
2023-02-16 09:36:13 -0500

    Node update

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/chat_application", {
+mongoose.connect("mongodb://127.0.0.1/chat_application", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
